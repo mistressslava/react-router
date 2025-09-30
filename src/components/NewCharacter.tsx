a49abea73f1999c132efbb75b40fc3a685de2572
Yaroslava <yaroslava.moskalova@gmail.com>
2025-10-01 00:06:48 +0200

    small updates

--- src/components/NewCharacter.tsx
+++ src/components/NewCharacter.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {FormEvent, useState} from "react";
 
 type NewCharacterProps = {
     name: string;
@@ -11,61 +11,64 @@ type NewCharacterProps = {
 
 export default function NewCharacter() {
 
-        const [character, setCharacter] = useState<NewCharacterProps>({
-            name: "",
-            status: "",
-            species: "",
-            gender: "",
-            image: ""
-        });
+    const [character, setCharacter] = useState<NewCharacterProps>({
+        name: "",
+        status: "",
+        species: "",
+        gender: "",
+        image: ""
+    });
+
+    function handleSubmit(event:FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log(character);
+    }
 
     return (
         <>
-            <div>
-                <form>
-                    <label> Name:
-                        <input
-                            value={character.name}
-                            onChange={(e) => setCharacter({...character, name: e.target.value})}
-                               placeholder={"Enter the name of a character..."}
-                        />
-                    </label>
-                </form>
-            </div>
-            <div>
-                <form>
-                    <label> Status:
-                        <input
-                            value={character.status}
-                            onChange={(e) => setCharacter({...character, status: e.target.value})}
-                               placeholder={"Enter the status of a character..."}
-                        />
-                    </label>
-                </form>
-            </div>
-            <div>
-                <form>
-                    <label> Gender:
-                        <input
-                            value={character.gender}
-                            onChange={(e) => setCharacter({...character, gender: e.target.value})}
-                               placeholder={"Enter the status of a character..."}
-                        />
-                    </label>
-                </form>
+            <form onSubmit={handleSubmit}>
+                <article>
+                    <div>
+                        <label> Name:
+                            <input
+                                value={character.name}
+                                onChange={(e) => setCharacter({...character, name: e.target.value})}
+                                placeholder={"Enter the name of a character..."}
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <label> Status:
+                            <input
+                                value={character.status}
+                                onChange={(e) => setCharacter({...character, status: e.target.value})}
+                                placeholder={"Enter the status of a character..."}
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <label> Gender:
+                            <input
+                                value={character.gender}
+                                onChange={(e) => setCharacter({...character, gender: e.target.value})}
+                                placeholder={"Enter the status of a character..."}
+                            />
+                        </label>
 
-            </div>
-            <div>
-                <form>
-                    <label> Image:
-                        <input
-                            value={character.image}
-                            onChange={(e) => setCharacter({...character, image: e.target.value})}
-                               placeholder={"Enter the link on a photo of a character..."}
-                        />
-                    </label>
-                </form>
-            </div>
+                    </div>
+                    <div>
+                        <label> Image:
+                            <input
+                                value={character.image}
+                                onChange={(e) => setCharacter({...character, image: e.target.value})}
+                                placeholder={"Enter the link on a photo of a character..."}
+                            />
+                        </label>
+
+                    </div>
+                </article>
+                <button type={"submit"}>Send</button>
+            </form>
         </>
     )
 }
